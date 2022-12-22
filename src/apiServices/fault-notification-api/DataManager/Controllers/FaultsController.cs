bfce1fd4a5f91c75b85e1e16411f2bf01a82e824
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2022-12-22 04:26:10 +0100

    Fault Notification API updated

---
+++ src/apiServices/fault-notification-api/DataManager/Controllers/FaultsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObject;
+
+namespace DataManager.Controllers
+{
+    [Route("api/faults")]
+    [ApiController]
+    public class FaultsController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+        public FaultsController(IServiceManager service) => _service = service;
+
+        [HttpGet]
+        public async Task<IActionResult> GetFaults()
+        {
+            var faults = await _service.FaultService.GetFaultsForReadAsync(trackChanges: false);
+            
+            return Ok(faults);
+        }
+
+        [HttpGet("{id:Guid}", Name = "FaultByID")]
+        public async Task<IActionResult> GetFault(Guid id)
+        {
+            var fault = await _service.FaultService.GetFaultsByIdAsync(id, trackChanges: false);
+            return Ok(fault);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFault([FromBody] FaultForCreationDto faultForCreation)
+        {
+            if (faultForCreation is null)
+                return BadRequest("FaultForCreationDto object is null");
+
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
+
+            var createdEntity = await _service.FaultService.FaultForCreationAsync(faultForCreation);
+
+            return CreatedAtRoute("FaultByID", new {id=createdEntity.Id}, createdEntity);
+        }
+    }
+}
---
+++ src/apiServices/fault-notification-api/FaultServiceApi/Migrations/20221221190250_initialMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FaultServiceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "fault",
+                columns: table => new
+                {
+                    FaultSerialNumber = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlazaId = table.Column<int>(type: "int", nullable: false),
+                    LaneNumber = table.Column<int>(type: "int", nullable: false),
+                    LaneDirection = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    EquipmentType = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FaultDescription = table.Column<string>(name: "Fault_Description", type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    LaneStatus = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeReported = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConfirmFaultStatus = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    DateTimeConfirmStatus = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fault", x => x.FaultSerialNumber);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fault");
+        }
+    }
+}
